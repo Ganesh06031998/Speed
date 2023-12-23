0f4bec964b1503cf238743a371824874d7c488b2
Ganesh <ganesh.a@prodapt.com>
2023-12-23 23:21:46 +0530

    Options Menu Implementation

--- src/app/individual-board/individual-board.component.ts
+++ src/app/individual-board/individual-board.component.ts
@@ -9,23 +9,34 @@ import { CsvFileService } from '../csvFile.service';
   styleUrls: ['./individual-board.component.css']
 })
 export class IndividualBoardComponent implements OnInit {
+  selectedOption: string = 'Overall Details';
+  userList: IndividualModel[] = [];
+  private overall = '../assets/IndividualLeaderboard.csv';
 
-  constructor(private dataService : DataService, private csvFileService : CsvFileService) { }
+  options = [
+    {value:'Overall Details', label:"Overall Details", path:'../assets/IndividualLeaderboard.csv'},
+    {value:'Unscramble', label:"Unscramble", path:'../assets/Unscramble.csv'},
+    {value:'Teasure Hunt', label:"Teasure Hunt", path:'../assets/TeasureHunt.csv'},
+    {value:'SPEED Rapid Fire Quiz', label:"SPEED Rapid Fire Quiz", path:'../assets/SPEEDRapidFireQuiz.csv'},
+    {value:'One Minute SPEED Story', label:"One Minute SPEED Story", path:'../assets/OneMinuteSPEEDStory.csv'},
+    {value:'SPEED Networking Event', label:"SPEED Networking Event", path:'../assets/SPEEDNetworkingEvent.csv'},
+    {value:'Learning and Devlopment Community', label:"Learning and Devlopment Community", path:'../assets/LearningandDevlopmentCommunity.csv'},
+    {value:'Flash Decision Making Challenge', label:"Flash Decision Making Challenge", path:'../assets/FlashDecisionMakingChallenge.csv'}
+  ];
+
+  constructor(private dataService: DataService, private csvFileService: CsvFileService) { }
 
   ngOnInit(): void {
     this.dataService.changeMessage('IndividualBoardComponent');
-    this.fetchCSVData();
+    this.onOptionChange();
   }
-
-  userList: IndividualModel[] = [];
-  private csvUrl = '../assets/IndividualLeaderboard.csv';
-  fetchCSVData(){
-    this.csvFileService.getCSVData(this.csvUrl).subscribe(
+  fetchCSVData(csvUrl : string) {
+    this.csvFileService.getCSVData(csvUrl).subscribe(
       (data: string) => {
-        console.log('data is '+data)
+        console.log('data is ' + data)
         this.userList = this.csvFileService.parseCSVDataIndividual(data);
-          console.log('After the CSV Data :::::: '+ this.userList);
-          this.getcsvData();
+        console.log('After the CSV Data :::::: ' + this.userList);
+        this.getcsvData();
       },
       (error) => {
         console.error('Error fetching CSV data:', error);
@@ -36,7 +47,7 @@ export class IndividualBoardComponent implements OnInit {
     console.log('All CSV Data:');
     this.userList.forEach((user: IndividualModel) => {
       console.log('Ecode:', user.ecode);
-      console.log('User Name:',  user.userName);
+      console.log('User Name:', user.userName);
       console.log('Score:', user.score);
       console.log('---------------------------');
     });
@@ -46,7 +57,17 @@ export class IndividualBoardComponent implements OnInit {
     if (user) {
       return `<a href="" class="text-decoration-none text-dark">${user.userName}</a>`;
     }
-  
+
     return "";
+  }
+
+  onOptionChange() {
+    console.log('Selected Option:', this.selectedOption);
+    this.options.forEach((option : any) => {
+      if(this.selectedOption === option.label){
+        console.log('selected path '+ option.path);
+        this.fetchCSVData(option.path);
+      }
+    });
   }
 }
