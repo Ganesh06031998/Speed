bfbd8ee55d282b2e03ca08e6bd4e5e60c1b06eda
Ganesh <ganesh.a@prodapt.com>
2023-12-23 21:32:38 +0530

    missed files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, UrlSegment } from '@angular/router';
 import { CsvFileService } from './csvFile.service';
-import { EventModel } from './shared/events.model';
-import { EventDetailsModel } from './shared/eventDetails.model';
 
 @Component({
   selector: 'app-root',
@@ -11,37 +9,13 @@ import { EventDetailsModel } from './shared/eventDetails.model';
 })
 export class AppComponent  {
   title = 'sample-testing';
-  csvEventsData: EventModel[] = [];
-  csvEventDetailsData: EventDetailsModel[] = [];
 
   constructor(private route: ActivatedRoute, private csvFileService : CsvFileService){
-
   }
 
   ngOnInit(){
     this.route.url.subscribe( UrlSegment=>{
       const currentUrl = UrlSegment.map(segment=>segment.path).join('/speed');
     })
-    this.fetchCSVData();
-  }
-
-  fetchCSVData(){
-    this.csvFileService.getEventsCSVData().subscribe(
-      (data: string) => {
-        this.csvEventsData = this.csvFileService.parseEventsCSVData(data);
-      },
-      (error) => {
-        console.error('Error fetching CSV data:', error);
-      }
-    )
-
-    this.csvFileService.getEventDetailsCSVData().subscribe(
-      (data : string) => {
-        this.csvEventDetailsData = this.csvFileService.parseEventDetailsCSVData(data);
-      },
-      (error) => {
-        console.error('Error fetching EventDatails CSV data:', error);
-      }
-    )
   }
 }
